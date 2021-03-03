5a29764f84cbf0435429d31dbfabf80d025d8b30
Paul Thomas <paulthomas2107@gmail.com>
2021-03-03 16:03:10 +0000

    More

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 import pygame
 
+# Init engine
+pygame.init()
+
+# Create screen
+screen = pygame.display.set_mode((800, 600))
+
+# Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
+# Game Loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # RGB
+    screen.fill((0, 0, 0))
+    pygame.display.update()
